b605c25399b7c08f1ebeaf01d39e1937813e6297
Vikash Pareek <vikash.pareek1509@gmail.com>
2020-12-23 10:57:26 +0530

    Initial Commit

---
+++ build/precache-manifest.e3ae7bb19ca053540344fa0748d6a492.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a8f828fd94bb320c33035960c5ea4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73d13c0d722ad3d05bfc",
+    "url": "/static/css/main.58be879b.chunk.css"
+  },
+  {
+    "revision": "b80a331ecd128f7de4d0",
+    "url": "/static/js/2.c3e67365.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c3e67365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73d13c0d722ad3d05bfc",
+    "url": "/static/js/main.552d8974.chunk.js"
+  },
+  {
+    "revision": "9f716d44a886be202ce9",
+    "url": "/static/js/runtime-main.20f95f91.js"
+  },
+  {
+    "revision": "0ddbac1cf431bf56dd8d71114f25e327",
+    "url": "/static/media/476.0ddbac1c.gif"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  }
+]);
